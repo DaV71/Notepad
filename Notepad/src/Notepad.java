6e3c25ed1bc9d34df42a6a0de4e77b340d9d9d2d
DaV71 <dsop13571@gmail.com>
2019-02-14 19:21:57 +0100

    Added function: Find, Find and Replace

--- Notepad/src/Notepad.java
+++ Notepad/src/Notepad.java
@@ -1,9 +1,8 @@
 import javax.swing.*;
+import javax.swing.event.InternalFrameAdapter;
+import javax.swing.event.InternalFrameEvent;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import java.awt.event.*;
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
@@ -14,14 +13,21 @@ public class Notepad extends JFrame {
     public  Notepad(){
         this.setVisible(true);
         this.setSize(500,600);
-        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
         this.setBounds((Toolkit.getDefaultToolkit().getScreenSize().width-this.getWidth())/2,(Toolkit.getDefaultToolkit().getScreenSize().height-this.getHeight())/2,this.getWidth(),this.getHeight());
-
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                saveFileHandler();
+                System.exit(0);
+            }
+        });
 
 
 
         this.setLayout(new GridLayout(1,1));
         this.getContentPane().add(scrollPane);
+
 
         initComponents();
 
@@ -32,6 +38,8 @@ public class Notepad extends JFrame {
 
         this.setJMenuBar(menuBar);
         JMenu file= menuBar.add(new JMenu("File"));
+        JMenu function = menuBar.add(new JMenu("Function"));
+
         JMenuItem newFile= file.add(new JMenuItem("New"));
         newFile.setAccelerator(KeyStroke.getKeyStroke("ctrl N"));
         newFile.setMnemonic('N');
@@ -50,6 +58,19 @@ public class Notepad extends JFrame {
         JMenuItem closeFile =  file.add(new JMenuItem("Close file"));
         closeFile.setAccelerator(KeyStroke.getKeyStroke("ctrl C"));
         closeFile.setMnemonic('C');
+
+
+        JMenuItem findSeq = function.add(new JMenuItem("Find"));
+        findSeq.setToolTipText("Find in text chars sequence ");
+        findSeq.setAccelerator(KeyStroke.getKeyStroke("ctrl F"));
+        findSeq.setMnemonic('F');
+        function.addSeparator();
+        JMenuItem replaceSeq = function.add(new JMenuItem("Find and Replace"));
+        replaceSeq.setToolTipText("Find in text chars sequence and replace them");
+        replaceSeq.setMnemonic('R');
+        replaceSeq.setAccelerator(KeyStroke.getKeyStroke("ctrl R"));
+
+
 
 
 
@@ -90,7 +111,22 @@ public class Notepad extends JFrame {
             }
         });
 
-       // panel1.add(scrollPane);
+        findSeq.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!isFindSelected)
+                    findSeqHandler();
+            }
+        });
+
+        replaceSeq.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(!isReplaceSelected)
+                    replaceSeqHandler();
+            }
+        });
+
 
 
     }
@@ -117,7 +153,7 @@ public class Notepad extends JFrame {
                 raf = new RandomAccessFile(file, "rwd");
                 String line = "";
                 while ((line=raf.readLine())!=null){
-                    textArea1.setText(textArea1.getText()+line);
+                    textArea1.setText(textArea1.getText()+"\n"+line);
 
                 }
 
@@ -192,12 +228,136 @@ public class Notepad extends JFrame {
            System.exit(0);}
     }
 
+    private void findSeqHandler(){
+        isFindSelected=true;
 
 
-    //private JPanel panel1= new JPanel();
+        internalFrameFind = new JInternalFrame("Find",false,true);
+        JPanel buttonPanel = new JPanel();
+        JButton findButton = new JButton("Find");
+        JLabel label = new JLabel("What are you looking for?");
+        internalFrameFind.setVisible(true);
+        internalFrameFind.setSize(new Dimension(200,140));
+        internalFrameFind.setLayout(new GridLayout(3,1));
+
+        textFind.setSize(new Dimension(100,7));
+        buttonPanel.add(findButton);
+        internalFrameFind.add(label);
+        internalFrameFind.getContentPane().add(textFind);
+        internalFrameFind.getContentPane().add(buttonPanel);
+
+
+        findButton.addActionListener(new FindSequence());
+
+        this.getContentPane().add(desktopPane);
+        desktopPane.add(internalFrameFind);
+        internalFrameFind.addInternalFrameListener(new InternalFrameAdapter() {
+            @Override
+            public void internalFrameClosed(InternalFrameEvent e) {
+                if (!isReplaceSelected)
+                    getContentPane().remove(desktopPane);
+                isFindSelected=false;
+                super.internalFrameClosed(e);
+            }
+
+        });
+    }
+
+    private void replaceSeqHandler(){
+        isReplaceSelected=true;
+        internalFrameReplace = new JInternalFrame("Find and Replace",false,true);
+        JPanel buttonPanel = new JPanel();
+        JButton findButton = new JButton("Replace");
+        JLabel label = new JLabel("What do you want replace?");
+        internalFrameReplace.setVisible(true);
+        internalFrameReplace.setSize(new Dimension(200,200));
+        internalFrameReplace.setLayout(new GridLayout(5,1));
+
+        textFindReplace.setSize(new Dimension(100,7));
+        buttonPanel.add(findButton);
+        internalFrameReplace.add(label);
+        internalFrameReplace.getContentPane().add(textFindReplace);
+        internalFrameReplace.getContentPane().add(new JLabel("New sequence:"));
+        internalFrameReplace.getContentPane().add(textReplace);
+        internalFrameReplace.getContentPane().add(buttonPanel);
+
+
+        findButton.addActionListener(new ReplaceSequence());
+
+        this.getContentPane().add(desktopPane);
+        desktopPane.add(internalFrameReplace);
+        internalFrameReplace.addInternalFrameListener(new InternalFrameAdapter() {
+            @Override
+            public void internalFrameClosed(InternalFrameEvent e) {
+
+                if (!isFindSelected)
+                    getContentPane().remove(desktopPane);
+
+                isReplaceSelected=false;
+
+                super.internalFrameClosed(e);
+            }
+        });
+
+    }
+
+    private class FindSequence implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+
+
+            if(firstIndex==-1||firstIndex==0)
+                firstIndex = textArea1.getText().indexOf(textFind.getText());
+
+            if (firstIndex>=0&&textFind.getText().length()>0){
+
+                textArea1.requestFocus();
+                textArea1.select(firstIndex,firstIndex+textFind.getText().length());
+                firstIndex = textArea1.getText().indexOf(textFind.getText(),firstIndex+textFind.getText().length());
+            }
+        }
+        private int firstIndex = 0;
+
+    }
+
+    private class ReplaceSequence implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+
+
+            if(firstIndexReplace==-1||firstIndexReplace==0)
+                firstIndexReplace = textArea1.getText().indexOf(textFindReplace.getText());
+
+            if (firstIndexReplace>=0&&textFindReplace.getText().length()>0){
+
+                textArea1.requestFocus();
+                textArea1.select(firstIndexReplace,firstIndexReplace+textFindReplace.getText().length());
+                textArea1.replaceSelection(textReplace.getText());
+                firstIndexReplace = textArea1.getText().indexOf(textFindReplace.getText(),firstIndexReplace+textFindReplace.getText().length());
+            }
+        }
+        private int firstIndexReplace = 0;
+
+    }
+
+
+
+
+
+    private JDesktopPane desktopPane = new JDesktopPane();
     private JTextArea textArea1 = new JTextArea();
     private  JScrollPane scrollPane = new JScrollPane(textArea1);
     private JMenuBar menuBar = new JMenuBar();
+    private JTextField textFind = new JTextField();
+    private JTextField textFindReplace = new JTextField();
+    private JTextField textReplace = new JTextField();
+    private  JInternalFrame internalFrameFind;
+    private  JInternalFrame internalFrameReplace;
+    private boolean isFindSelected = false;
+    private boolean isReplaceSelected = false;
+
 
     public static void main(String[] args) {
         new Notepad();
